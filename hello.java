bdb5451aff9cceff1ef4c36726c2eb2650742c06
Ayush Kumar <146207142+ayushkzz@users.noreply.github.com>
2026-01-08 10:02:38 +0530

    Uploaded test file

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class HelloJenkins {
+
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("✅ Jenkins is working successfully!");
+        System.out.println("Java program executed without errors.");
+        System.out.println("=================================");
+    }
+}
