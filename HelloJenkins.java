0dc47f8e6722e89454f0a91cb52fbf7edd00a3da
Ayush Kumar <146207142+ayushkzz@users.noreply.github.com>
2026-01-09 09:48:16 +0530

    Update message to include ngrok public URL

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -2,7 +2,8 @@ public class HelloJenkins {
 
     public static void main(String[] args) {
         System.out.println("Jenkins is working successfully!");
-        System.out.println("Hello Jenkins. The Jenkins CI is now working using webhooks.");
+        System.out.println("Hello Jenkins. The Jenkins CI is now working using github webhooks and ngrok public url.");
     }
 }
+
 
