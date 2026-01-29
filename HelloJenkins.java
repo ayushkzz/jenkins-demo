14e1520a8691fd91bf3a2d172ee6e6a36afd72b9
Ayush Kumar <146207142+ayushkzz@users.noreply.github.com>
2026-01-29 19:56:50 +0530

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -2,8 +2,9 @@ public class HelloJenkins {
 
     public static void main(String[] args) {
         System.out.println("Jenkins is working successfully!");
-        System.out.println("Hello Jenkins. The Jenkins CI is now working using github webhooks and ngrok public url.");
+        System.out.println("Hello Jenkins. Poll SCM every minute.");
     }
 }
+
 
 
