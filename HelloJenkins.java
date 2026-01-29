fa303e11763f5f689a52918a745694eb3b977837
Ayush Kumar <146207142+ayushkzz@users.noreply.github.com>
2026-01-29 19:58:25 +0530

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -2,9 +2,10 @@ public class HelloJenkins {
 
     public static void main(String[] args) {
         System.out.println("Jenkins is working successfully!");
-        System.out.println("Hello Jenkins. Poll SCM every minute.");
+        System.out.println("Hello Jenkins. Poll SCM every minute on change.");
     }
 }
+
 
 
 
