a1b933bc980e83a0bd5c7f32e6b0dcf23e9a68cc
Ayush Kumar <146207142+ayushkzz@users.noreply.github.com>
2026-01-30 00:19:08 +0530

    Update webhook message in HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -2,9 +2,10 @@ public class HelloJenkins {
 
     public static void main(String[] args) {
         System.out.println("Jenkins is working successfully!");
-        System.out.println("Hello Jenkins. Webhook automation again.");
+        System.out.println("Hello Jenkins. Webhook using NGROK.");
     }
 }
+
 
 
 
